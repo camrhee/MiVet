c858a89f677b30f62271d6646aa4ef5dd75b02b7
camrhee <109054280+camrhee@users.noreply.github.com>
2022-10-22 15:55:46 -0700

    Create Service.cs

---
+++ .NET/Models/Service.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.VetProfiles
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public int ServiceTypeId { get; set; }
+    }
+}
