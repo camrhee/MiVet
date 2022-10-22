6d862d9f80837cbaa2e261be3600a76d4e9e0167
camrhee <109054280+camrhee@users.noreply.github.com>
2022-10-22 15:49:23 -0700

    Create VetProfileUpdateRequest.cs

---
+++ .NET/Services/VetProfileUpdateRequest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.VetProfiles
+{
+    public class VetProfileUpdateRequest : VetProfileAddRequest , IModelIdentifier
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Id { get; set; }
+    }
+}
