012b631dedc6fdf4031a86b94fa0a58a251939b8
camrhee <109054280+camrhee@users.noreply.github.com>
2023-04-14 13:31:55 -0700

    Update Location.cs

--- .NET/Models/Location.cs
+++ .NET/Models/Location.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models.Domain.Locations
+namespace MiVet.Models.Domain.Locations
 {
     public class Location
     {
