953a1149a6af8d94c0e0549aa3fc6833771586c9
camrhee <109054280+camrhee@users.noreply.github.com>
2023-04-16 20:13:45 -0700

    Update VetProfileAddRequest.cs

--- .NET/Services/VetProfileAddRequest.cs
+++ .NET/Services/VetProfileAddRequest.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sabio.Models.Domain.Locations;
-using Sabio.Models.Domain.Practices;
+using MiVet.Models.Domain.Locations;
+using MiVet.Models.Domain.Practices;
 
-namespace Sabio.Models.Domain.VetProfiles
+namespace MiVet.Models.Domain.VetProfiles
 {
     public class VetProfileV2
     {
